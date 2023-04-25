46a2f1c2c362d0bddbaf362621e9ab1dcab2c0ea
msovet <merei9822@gmail.com>
2023-04-25 15:09:36 +0600

    Initial commit

---
+++ src/main/java/kz/msovet/springmicroservice2/SpringMicroservice2Application.java
@@ -0,0 +1,13 @@
+package kz.msovet.springmicroservice2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMicroservice2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMicroservice2Application.class, args);
+    }
+
+}
---
+++ src/test/java/kz/msovet/springmicroservice2/SpringMicroservice2ApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.msovet.springmicroservice2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMicroservice2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
